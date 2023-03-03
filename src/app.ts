1689afcb985eb8a418592620301ea4783c4b292e
Carlos <carua97@gmail.com>
2023-03-03 23:02:32 +0100

    config add and app modified

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import createDebug from 'debug';
 import morgan from 'morgan';
 import cors from 'cors';
+import { CustomError } from './errors/errors';
 
+const debug = createDebug('RS:App');
 export const app = express();
-app.disable('x-powered-by');
 
-// Const debug = createDebug('Fruits');
+app.disable('x-powered-by');
 const corsOptions = {
   origin: '*',
 };
@@ -14,9 +15,29 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors(corsOptions));
 
+// App different paths
+
 app.get('/', (_req, resp) => {
   resp.json({
-    info: '/Esta es una prueba',
+    info: 'Main',
     endpoints: {},
   });
 });
+
+app.use(
+  (error: CustomError, _req: Request, resp: Response, _next: NextFunction) => {
+    debug('Middleware de errores');
+    const status = error.statusCode || 500;
+    const statusMessage = error.statusMessage || 'Internal server error';
+    resp.status(status);
+    resp.json({
+      error: [
+        {
+          status,
+          statusMessage,
+        },
+      ],
+    });
+    debug(status, statusMessage, error.message);
+  }
+);
