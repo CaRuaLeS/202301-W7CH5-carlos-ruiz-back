f0133451cfaaac5c082e1ef0558d16de11d27e93
Carlos <carua97@gmail.com>
2023-03-03 23:44:25 +0100

    eclint fix files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import http from 'http';
 import { app } from './app.js';
 import createDebug from 'debug';
+import { dbConnect } from './db/db.connect.js';
+import mongoose from 'mongoose';
 
 const debug = createDebug('RS');
 
@@ -8,7 +10,16 @@ const PORT = process.env.PORT || 4500;
 
 const server = http.createServer(app);
 
-server.listen(PORT);
+dbConnect()
+  .then((mongoose) => {
+    server.listen(PORT);
+    debug('BD ', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => server.emit('error', error));
+
+server.on('error', (error) => {
+  debug('Server error ', error.message);
+});
 
 server.on('listening', () => {
   debug('Listening in http://localhost:' + PORT);
