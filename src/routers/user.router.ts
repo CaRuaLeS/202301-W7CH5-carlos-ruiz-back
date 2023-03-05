4acb0f1a00c9e95a29c7e98397c097dfb7c45f40
Carlos <carua97@gmail.com>
2023-03-05 11:11:45 +0100

    Add interceptors

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,5 +1,6 @@
 import { Router as router } from 'express';
 import { UserController } from '../controller/user.controller.js';
+import { logged } from '../interceptors/logged.js';
 import { UserMongoRepo } from '../repository/user.mongo.repo.js';
 
 export const usersRouter = router();
@@ -8,4 +9,10 @@ const controller = new UserController(repo);
 
 usersRouter.post('/register', controller.register.bind(controller));
 usersRouter.post('/login', controller.login.bind(controller));
-usersRouter.patch('/add_friend/:id', controller.addFriend.bind(controller));
+
+usersRouter.get('/', logged, controller.getAll.bind(controller));
+usersRouter.patch(
+  '/add_friend/:id',
+  logged,
+  controller.addFriend.bind(controller)
+);
